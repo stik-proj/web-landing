7282b7060a1dcccd6a4f9b3b71c2a62c63fc694c
EZ-woong <tjsdnd3955@naver.com>
2024-01-12 10:59:10 +0900

    - 팀 변경 - 파트너사 추가

--- static/js/ui/common.js
+++ static/js/ui/common.js
@@ -32,154 +32,175 @@ const teamListLeader = [
     name: "Yongjae Kim",
     position: "CMO | Youtube Influencer",
     youtubeTag: ["@yongjeabro", "@ap5798"],
-    youtubeUrl: ["https://www.youtube.com/@yongjaebro", "https://www.youtube.com/@ap5798"],
+    youtubeUrl: [
+      "https://www.youtube.com/@yongjaebro",
+      "https://www.youtube.com/@ap5798",
+    ],
     sub: "490K subscribers",
   },
 ];
-const teamListStrategic = [
+const teamListStrategyDesign = [
   {
-    imgUrl: "/static/img/member/strategic/jinhong.png",
-    name: "Jinhong Sung",
-    position: "Planning",
-  },
-  {
-    imgUrl: "/static/img/member/strategic/eunmin.png",
+    imgUrl: "/static/img/member/strategy design/Thomas Gil.png",
     name: "Thomas Gil",
+    position: "PO",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Junyoung Park.png",
+    name: "Junyoung Park",
+    position: "PM",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Hoyoung Kim.png",
+    name: "Hoyoung Kim",
     position: "Planning",
   },
   {
-    imgUrl: "/static/img/member/strategic/kitae.png",
-    name: "Kitae Kwon",
-    position: "Planning",
-  },
-  {
-    imgUrl: "/static/img/member/strategic/jinho.png",
-    name: "Jinho Lee",
-    position: "Development",
-  },
-  {
-    imgUrl: "/static/img/member/strategic/youngsoo.png",
-    name: "Youngsoo Won",
-    position: "Development",
-  },
-  {
-    imgUrl: "/static/img/member/strategic/jaehyo.png",
-    name: "Jaehyo Lim",
-    position: "Development",
-  },
-  {
-    imgUrl: "/static/img/member/strategic/bokyun.png",
-    name: "Bokyun kim",
-    position: "Development",
-  },
-  {
-    imgUrl: "/static/img/member/strategic/onyu.png",
-    name: "onyu Jeong",
-    position: "Development",
-  },
-  {
-    imgUrl: "/static/img/member/strategic/sunghwi.png",
-    name: "Sunghwi Hong",
-    position: "Development",
-  },
-  {
-    imgUrl: "/static/img/member/strategic/gijae.png",
-    name: "Liam Lee",
-    position: "Development",
-  },
-];
-const teamListBlockChain = [
-  {
-    imgUrl: "/static/img/member/blockchain/scott.png",
-    name: "Scott Jeong",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/samuel.png",
-    name: "Samuel Kim",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/dean.png",
-    name: "Dean Kang",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/juan.png",
-    name: "Juan Park",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/haemil.png",
-    name: "Haemil Cho",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/jay.png",
-    name: "Jay Shin",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/eunsun.png",
-    name: "Eunsun Kim",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/buddy.png",
-    name: "Buddy Hwang",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/robert.png",
-    name: "Robert Baik",
-    position: "Tech LAB",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/junyoung.png",
-    name: "Junyoung park",
-    position: "Planning",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/dawson.png",
+    imgUrl: "/static/img/member/strategy design/Dawson Lee.png",
     name: "Dawson Lee",
     position: "Design",
   },
   {
-    imgUrl: "/static/img/member/blockchain/sia.png",
-    name: "Sia Park",
-    position: "Design",
-  },
-  {
-    imgUrl: "/static/img/member/blockchain/jiyun.png",
+    imgUrl: "/static/img/member/strategy design/Jiyun Lim.png",
     name: "Jiyun Lim",
     position: "Design",
   },
   {
-    imgUrl: "/static/img/member/blockchain/kaya.png",
+    imgUrl: "/static/img/member/strategy design/Jieun Lee.png",
+    name: "Jieun Lee",
+    position: "Design",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Yisoo Kim.png",
+    name: "Yisoo Kim",
+    position: "Design",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Kaya Cheon.png",
     name: "Kaya Cheon",
     position: "Design",
   },
-];
-const teamListMarketingPeople = [
   {
-    imgUrl: "/static/img/member/marketingPeople/byungryeol.png",
-    name: "Byungryeol Min",
-    position: "People",
-  },
-  {
-    imgUrl: "/static/img/member/marketingPeople/hyeonseok.png",
-    name: "Hyeonseok Cho",
-    position: "Marketing",
-  },
-  {
-    imgUrl: "/static/img/member/marketingPeople/kyunghoon.png",
+    imgUrl: "/static/img/member/strategy design/Kyunghoon Yeom.png",
     name: "Kyunghoon Yeom",
     position: "Marketing",
   },
   {
-    imgUrl: "/static/img/member/marketingPeople/jeongyeon.png",
+    imgUrl: "/static/img/member/strategy design/Jeongyeon Moon.png",
     name: "Jeongyeon Moon",
     position: "Marketing",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Eunyoung Song.png",
+    name: "Eunyoung Song",
+    position: "QA/QC",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Youngjun Park.png",
+    name: "Youngjun Park",
+    position: "GM",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Jinhong Sung.png",
+    name: "Jinhong Sung",
+    position: "Strategy",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Byungryeol Min.png",
+    name: "Byungryeol Min",
+    position: "People",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Kitae Kwon.png",
+    name: "Kitae Kwon",
+    position: "Accountancy",
+  },
+  {
+    imgUrl: "/static/img/member/strategy design/Hyeonseok Cho.png",
+    name: "Hyeonseok Cho",
+    position: "Cooperation",
+  },
+];
+const teamListDevelopment = [
+  {
+    imgUrl: "/static/img/member/tech development/Scott Jeong.png",
+    name: "Scott Jeong",
+    position: "DEV Lead",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Youngsoo Won.png",
+    name: "Youngsoo Won",
+    position: "DEV Lead",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Dean Kang.png",
+    name: "Dean Kang",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Juan Park.png",
+    name: "Juan Park",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Buddy Hwang.png",
+    name: "Buddy Hwang",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Robert Baik.png",
+    name: "Robert Baik",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Samuel Kim.png",
+    name: "Samuel Kim",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Russell Ahn.png",
+    name: "Russell Ahn",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Haemil Cho.png",
+    name: "Haemil Cho",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Eunsun Kim.png",
+    name: "Eunsun Kim",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Jay Shin.png",
+    name: "Jay Shin",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Jinho Lee.png",
+    name: "Jinho Lee",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Sunghwi Hong.png",
+    name: "Sunghwi Hong",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Young Jang.png",
+    name: "Young Jang",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Bokyun kim.png",
+    name: "Bokyun kim",
+    position: "Development",
+  },
+  {
+    imgUrl: "/static/img/member/tech development/Liam Lee.png",
+    name: "Liam Lee",
+    position: "Development",
   },
 ];
 $.fn.memberInfoInput = function (memberlist) {
@@ -227,15 +248,47 @@ ui.page.common.prototype = {
   _assignElements: function () {
     this.welDoc = $(document.body);
     this.welWin = $(window);
-    this.welDoc.on("click", 'a[href="#"]', $.proxy(this._onClickEventPrevent, this));
-    this.welDoc.on("click", "._toggleTranslateMenu", $.proxy(this._toggleTranslatebox, this));
-    this.welDoc.on("click", "._openMobileGnbMenu", $.proxy(this._openMobileGnbMenu, this));
-    this.welDoc.on("click", "._btnStaikaAppDownload", $.proxy(this._moveStaikaDownloadAppPage, this));
-    this.welDoc.on("click", "._btnGazagoAppDownload", $.proxy(this._moveGazagoDownloadAppPage, this));
-    this.welDoc.on("click", ".gnb li a", $.proxy(this._closeMobileGnbMenu, this));
+    this.welDoc.on(
+      "click",
+      'a[href="#"]',
+      $.proxy(this._onClickEventPrevent, this)
+    );
+    this.welDoc.on(
+      "click",
+      "._toggleTranslateMenu",
+      $.proxy(this._toggleTranslatebox, this)
+    );
+    this.welDoc.on(
+      "click",
+      "._openMobileGnbMenu",
+      $.proxy(this._openMobileGnbMenu, this)
+    );
+    this.welDoc.on(
+      "click",
+      "._btnStaikaAppDownload",
+      $.proxy(this._moveStaikaDownloadAppPage, this)
+    );
+    this.welDoc.on(
+      "click",
+      "._btnGazagoAppDownload",
+      $.proxy(this._moveGazagoDownloadAppPage, this)
+    );
+    this.welDoc.on(
+      "click",
+      ".gnb li a",
+      $.proxy(this._closeMobileGnbMenu, this)
+    );
     this.welDoc.on("click", ".gnb li a", $.proxy(this._initTeamScroll, this));
-    this.welDoc.on("click", "._closeMobileGnbMenu", $.proxy(this._closeMobileGnbMenu, this));
-    this.welDoc.on("click", ".teamNav ul li", $.proxy(this._onClickScrollMove, this));
+    this.welDoc.on(
+      "click",
+      "._closeMobileGnbMenu",
+      $.proxy(this._closeMobileGnbMenu, this)
+    );
+    this.welDoc.on(
+      "click",
+      ".teamNav ul li",
+      $.proxy(this._onClickScrollMove, this)
+    );
   },
   _onScrollEvent: function () {
     // $(".section06").on("mousewheel", function (e, a) {
@@ -246,26 +299,43 @@ ui.page.common.prototype = {
     event.preventDefault();
   },
   _attachEventHandlers: function () {
-    this.welDoc.on("click", 'a[href="#"]', $.proxy(this._onClickEventPrevent, this));
+    this.welDoc.on(
+      "click",
+      'a[href="#"]',
+      $.proxy(this._onClickEventPrevent, this)
+    );
   },
   _moveStaikaDownloadAppPage: function () {
     if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
       window.open("https://apps.apple.com/us/app/staika/id1629354285");
     } else {
-      window.open("https://play.google.com/store/apps/details?id=kr.co.eztechfin.staika_app");
+      window.open(
+        "https://play.google.com/store/apps/details?id=kr.co.eztechfin.staika_app"
+      );
     }
   },
   _moveGazagoDownloadAppPage: function () {
     if (navigator.userAgent.match(/(iPod|iPhone|iPad)/)) {
       window.open("https://testflight.apple.com/join/yNZhHo9Z");
     } else {
-      window.open("https://play.google.com/store/apps/details?id=kr.co.eztechfin.gazaGo");
+      window.open(
+        "https://play.google.com/store/apps/details?id=kr.co.eztechfin.gazaGo"
+      );
     }
   },
   _initTeamScroll: function (e) {
-    const insTeam = $.fn.fp_scrolloverflow.iscrollHandler.iScrollInstances.filter((item) => item.wrapper.innerText.includes("Team"));
-    const insRoadmap = $.fn.fp_scrolloverflow.iscrollHandler.iScrollInstances.filter((item) => item.wrapper.innerText.includes("Roadmap"));
-    const insExperience = $.fn.fp_scrolloverflow.iscrollHandler.iScrollInstances.filter((item) => item.wrapper.innerText.includes("Experience"));
+    const insTeam =
+      $.fn.fp_scrolloverflow.iscrollHandler.iScrollInstances.filter((item) =>
+        item.wrapper.innerText.includes("Team")
+      );
+    const insRoadmap =
+      $.fn.fp_scrolloverflow.iscrollHandler.iScrollInstances.filter((item) =>
+        item.wrapper.innerText.includes("Roadmap")
+      );
+    const insExperience =
+      $.fn.fp_scrolloverflow.iscrollHandler.iScrollInstances.filter((item) =>
+        item.wrapper.innerText.includes("Experience")
+      );
 
     if (e.currentTarget.innerText === "Roadmap") {
       insRoadmap[0].scrollTo(0, 0);
@@ -289,7 +359,9 @@ ui.page.common.prototype = {
     const target = $(e.currentTarget).data("target");
     const elTeam = $("#" + target);
 
-    const ins = $.fn.fp_scrolloverflow.iscrollHandler.iScrollInstances.filter((item) => item.wrapper.innerText.includes("Team"));
+    const ins = $.fn.fp_scrolloverflow.iscrollHandler.iScrollInstances.filter(
+      (item) => item.wrapper.innerText.includes("Team")
+    );
 
     // $(".teamNav li").addClass("current").siblings().removeClass("current");
 
@@ -305,11 +377,33 @@ ui.page.common.prototype = {
   },
   initFullpage: function () {
     let needToMoveToStaika = window.location.search.includes("staikaApp");
-    const scrollPosition = $("#StaikaApp").outerHeight() - $("#section5").outerHeight();
+    const scrollPosition =
+      $("#StaikaApp").outerHeight() - $("#section5").outerHeight();
 
     $("#fullpage").fullpage({
-      sectionsColor: ["", "#fff", "#fff", "#fff", "#fff", "#fff", "#fff", "#fff", "#fff", "#fff"],
-      anchors: ["firstPage", "secondPage", "3rdPage", "4thpage", "5thPage", "6thPage", "7thPage", "8thPage", "lastPage"],
+      sectionsColor: [
+        "",
+        "#fff",
+        "#fff",
+        "#fff",
+        "#fff",
+        "#fff",
+        "#fff",
+        "#fff",
+        "#fff",
+        "#fff",
+      ],
+      anchors: [
+        "firstPage",
+        "secondPage",
+        "3rdPage",
+        "4thpage",
+        "5thPage",
+        "6thPage",
+        "7thPage",
+        "8thPage",
+        "lastPage",
+      ],
       menu: "#menu",
       // autoScrolling: false,
       slidesNavigation: true,
@@ -325,7 +419,17 @@ ui.page.common.prototype = {
         console.log(section, slide, position, direction);
       },
       afterSlideLoad: function (anchorLink, index, slideAnchor, slideIndex) {
-        console.log("afterSlideLoad--" + "anchorLink: " + anchorLink + " index: " + index + " slideAnchor: " + slideAnchor + " slideIndex: " + slideIndex);
+        console.log(
+          "afterSlideLoad--" +
+            "anchorLink: " +
+            anchorLink +
+            " index: " +
+            index +
+            " slideAnchor: " +
+            slideAnchor +
+            " slideIndex: " +
+            slideIndex
+        );
         console.log("----------------");
       },
       afterLoad: function (anchorLink, index) {
@@ -374,9 +478,9 @@ ui.page.common.prototype = {
       ],
     });
     // $(".teamListLeader").memberInfoInput(teamListLeader);
-    $(".teamListStrategic").memberInfoInput(teamListStrategic);
-    $(".teamListBlockChain").memberInfoInput(teamListBlockChain);
-    $(".teamListMarketingPeople").memberInfoInput(teamListMarketingPeople);
+    $(".teamListStrategyDesign").memberInfoInput(teamListStrategyDesign);
+    $(".teamListDevelopment").memberInfoInput(teamListDevelopment);
+    // $(".teamListMarketingPeople").memberInfoInput(teamListMarketingPeople);
   },
 };
 var uiCommon = new ui.page.common();
